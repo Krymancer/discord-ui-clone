d3b5ea9990eafcd43884284d1709f8dd3f7b4cf5
krymancer <junior.nascm@gmail.com>
2020-09-02 01:05:59 -0300

    Updates

---
+++ precache-manifest.8852cd6ed6c58caff47ac1d14dbd526f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2774a3d9016b4bf30ed375a5e7df932f",
+    "url": "/discord-ui-clone/index.html"
+  },
+  {
+    "revision": "0b1fa0e4a65ba7e5dacf",
+    "url": "/discord-ui-clone/static/js/2.b42d7039.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/discord-ui-clone/static/js/2.b42d7039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a36f5f0e17be6e882b80",
+    "url": "/discord-ui-clone/static/js/main.c1607f00.chunk.js"
+  },
+  {
+    "revision": "6240a96580aa4e9f2812",
+    "url": "/discord-ui-clone/static/js/runtime-main.454bc4d9.js"
+  },
+  {
+    "revision": "3fbf5a84d4f02dda562cd0a2775e6727",
+    "url": "/discord-ui-clone/static/media/Logo.3fbf5a84.svg"
+  }
+]);
