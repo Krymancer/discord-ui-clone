f3f2aaec47fde507d9e1c750e0252b915eebf9f0
krymancer <junior.nascm@gmail.com>
2020-09-02 01:01:56 -0300

    Push

---
+++ README.md
@@ -0,0 +1,7 @@
+# Discord React UI Clone
+
+A clone of Discord UI using ReacJS and Typescript [Deployed here](https://krymancer.github.io/discord-ui-clone/)
+
+<p align="center">
+  <img alt="Discord" src=".github/discord.png">
+</p>%
---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,51 @@
+import React, { useRef, useEffect } from 'react';
+
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
+
+const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
+
+  return (
+    <Container>
+      <Messages ref={messagesRef}>
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage
+            key={n}
+            author="Júnior"
+            date="01/03/2020"
+            content="Hoje é o meu aniversário!"
+          />
+        ))}
+
+        <ChannelMessage
+          author="Bot"
+          date="01/03/2020"
+          content={
+            <>
+              <Mention>@Júnior</Mention>, Parabéns.
+            </>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
+
+      <InputWrapper>
+        <Input type="text" placeholder="Conversarem #chat-livre" />
+        <InputIcon />
+      </InputWrapper>
+    </Container>
+  );
+};
+
+export default ChannelData;
